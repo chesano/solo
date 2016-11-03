a52c853bba3967ad61845202ed713d3753481bfd
chesano <chetsvr@gmail.com>
2016-11-03 14:09:40 +0530

    test

--- HelloWorldBot.py
+++ HelloWorldBot.py
@@ -14,7 +14,7 @@ def webhook():
       data = json.loads(request.data)
       text = data['entry'][0]['messaging'][0]['message']['text'] # Incoming Message Text
       sender = data['entry'][0]['messaging'][0]['sender']['id'] # Sender ID
-      payload = {'recipient': {'id': sender}, 'message': {'text': "Hello World"}} # We're going to send this back
+      payload = {'recipient': {'id': sender}, 'message': {'text': "Heya1.."}} # We're going to send this back
       r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload) # Lets send it
     except Exception as e:
       print traceback.format_exc() # something went wrong
@@ -22,7 +22,7 @@ def webhook():
     if request.args.get('hub.verify_token') == 'chesanotoken':
       return request.args.get('hub.challenge')
     return "Wrong Verify Token"
-  return "Hello World" #Not Really Necessary
+  return "Heya" #Not Really Necessary
 
 if __name__ == '__main__':
   app.run(debug=True)
